2b4084802f7255e6a709c28dfa42498a00fe0af5
Janos Kocs <janos.kocs@outlook.com>
2023-06-05 17:42:31 +0100

    Fix char at in string

--- src/utils/generatePalletString.js
+++ src/utils/generatePalletString.js
@@ -2,12 +2,12 @@ export const generatePalletString = (service, date, hour, pageCount = '00') => {
     let stringArr = []
 
     if (pageCount === 0) {
-        stringArr.push(`PAL${service}${date}${hour}${pageCount}`)
+        stringArr.push(`PAL${service.charAt(0)}${date}${hour}${pageCount}`)
     }
 
     for (let i = 0; i < pageCount; i++) {
         const palletNum = i < 10 ? `0${i}` : `${i}`; // Add leading zero for values less than 10
-        stringArr.push(`PAL${service}${date}${hour}${palletNum}`)
+        stringArr.push(`PAL${service.charAt(0)}${date}${hour}${palletNum}`)
     }
     return stringArr
 };
