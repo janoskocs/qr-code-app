17670e1a9e8de2824cc47158b406f398ee3a844c
Janos Kocs <janos.kocs@outlook.com>
2023-07-04 22:10:54 +0100

    Update generate qr code to start with number 1

--- src/utils/generatePalletString.js
+++ src/utils/generatePalletString.js
@@ -1,11 +1,11 @@
-export const generatePalletString = (service, date, hour, pageCount = '00') => {
+export const generatePalletString = (service, date, hour, pageCount = 1) => {
     let stringArr = []
 
-    if (pageCount == 0) {
-        stringArr.push(`PAL${service.charAt(0)}${date}${hour}${pageCount}`)
+    if (pageCount === 0) {
+        stringArr.push(`PAL${service.charAt(0)}${date}${hour}01`)
     }
 
-    for (let i = 0; i < pageCount; i++) {
+    for (let i = 1; i <= pageCount; i++) {
         const palletNum = i < 10 ? `0${i}` : `${i}`; // Add leading zero for values less than 10
         stringArr.push(`PAL${service.charAt(0)}${date}${hour}${palletNum}`)
     }
